1c81a428f80cc51cba711e6fd7dd610f5271cab6
EC2 Default User <ec2-user@ip-172-31-42-115.ec2.internal>
2019-03-25 22:35:34 +0000

    Downloading from S3 and video chunking now works.

--- Download_S3.py
+++ Download_S3.py
@@ -3,14 +3,38 @@ Script to download a video from the relevant amazon S3 bucket into a temporary d
 '''
 import sys
 import boto3 
+from boto3.s3.transfer import S3Transfer 
 import botocore 
+import threading
+## from https://stackoverflow.com/questions/41827963/track-download-progress-of-s3-file-using-boto3-and-callbacks
+class ProgressPercentage(object):
+    def __init__(self,client,BUCKET,KEY):
+        self._filename = KEY
+        self._size = client.head_object(Bucket=BUCKET,Key=KEY)['ContentLength']
+        self._seen_so_far = 0
+        self._lock = threading.Lock()
+
+    def __call__(self,bytes_amount):
+        with self._lock:
+            self._seen_so_far += bytes_amount
+            percentage = round((self._seen_so_far/self._size)*100,2)
+            sys.stdout.write(
+                        "\r%s  %s / %s  (%.2f%%)" % (
+                        self._filename, self._seen_so_far, self._size,
+                        percentage))
+            sys.stdout.flush()
+
 
 def download(BUCKET_NAME,KEY):
 
     s3 = boto3.resource('s3')
+    # for the purposes of temporary storage, we only use the last bit of the name as an indentifier: 
+    USEKEY = KEY.split('/')[-1]
 
     try:
-        s3.Bucket(BUCKET_NAME).download_file(KEY, '~/tmp/'+KEY)
+        transfer = S3Transfer(boto3.client('s3','us-east-1')) 
+        progress = ProgressPercentage(transfer._manager._client,BUCKET_NAME,KEY)
+        transfer.download_file(BUCKET_NAME,KEY, '../vmnt/tmp_videos/'+USEKEY,callback = progress)
     except botocore.exceptions.ClientError as e:
         if e.response['Error']['Code'] == "404":
             print("The object does not exist.")
@@ -20,7 +44,7 @@ def download(BUCKET_NAME,KEY):
 if __name__ == "__main__":
     key = sys.argv[1]
     bucket_name = 'froemkelab.videodata'
-    download(bucket_name,key):
+    download(bucket_name,key)
 
 
 
--- Examine_frames.py
+++ Examine_frames.py
@@ -10,9 +10,18 @@ import os
 from moviepy.editor import VideoFileClip
 import numpy as np
 import sys
+import multiprocessing 
 import subprocess
 import re
 import gc
+
+# helper function for parallelization: 
+def index_segments(segments,vcpus):
+    nb_partition = np.max((len(segments)//vcpus,1))
+    list_parts  = [segments[i*nb_partition:(i+1)*nb_partition] for i in range((len(segments)+nb_partition-1)//nb_partition)]
+    return list_parts
+
+
 def inspect_videos(cwd = None):
 
     if cwd is None:
@@ -106,7 +115,7 @@ def crop_videos(cwd = None):
                  print(e.errno)
                  print('configuration not loaded')
 
-def crop_videos_local(cwd = None,localdir):
+def crop_videos_local(localdir,cwd = None):
     length = 1200
     if cwd is None:
         # First get current directory
@@ -236,7 +245,6 @@ def crop_videos_special(videopath,videoname,directorypath):
                 print('segment not viable')
             gc.collect()
     except OSError as e:
-
          print(e.errno)
          print('configuration not loaded')
 
@@ -265,7 +273,156 @@ def compress_videos(cwd = None):
             else:
                 print('could not compress '+cwd+'/'+sub+'/'+video)
 
+def crop_videos_v2(cwd = None):
+    # length in seconds of each video 
+    length = 1200
+    if cwd is None:
+        # First get current directory
+        cwd = os.getcwd()
+    # First get all subdirectories:
+    files = os.listdir(cwd)
+    videos = [video for video in files if video.split('.')[-1] == 'avi']
+    print(videos)
+    for video in videos:
+        ## Load in cropped version of
+        print('loading ' +video)
+        clip = VideoFileClip(cwd+'/'+video)
+        try:
+            with open(cwd+'/'+video.split('.')[0]+'config.py','r+') as f:
+                coords = ['x0 = \n','y0 = \n','x1 = \n','y1 = \n']
+                intcoords = []
+                for coord in range(len(coords)):
+                    coords[coord] = f.readline()
+                    nums = re.findall('\d+',coords[coord])[1]
+                    intcoords.append(nums)
+
+            print(video.split('.')[0]+'cropped.avi')
+            cropped = clip.crop(x1 = intcoords[0],y1 = intcoords[1],x2 = intcoords[2],y2 = intcoords[3])
+            ## We want to split our video into manageable segments.
+            ## Account for the case that our video analysis failed somewhere in the middle:
+            # We want to be able to extract out the things that have been done so far:
+
+            ## First get the duration in seconds:
+            seconds = cropped.duration
+            # If analysis has been found:
+            ident =  video.split('.')[0]+'cropped_'+'part'
+            print(files,ident)
+            print([part for part in files if ident in part.split('.')[0]])
+            done = [int(re.findall('\d+',part.split('.')[0])[-1]) for part in files if ident in part.split('.')[0]]
+            presegs = range(np.ceil(seconds/length).astype(int))
+            if len([part for part in files if ident in part.split('.')[0]]):
+                segments = [segment for segment in presegs if segment not in done]
+            else:
+                segments = presegs 
+            for segment in segments:
+                try:
+                    # Ensures that the last clip is the right length
+                    print("producing segment "+str(segment) + 'of ' + str(segments))
+                    if segment == presegs[-1]:
+
+                        endseg = -1
+                    else:
+                        endseg = length*(segment+1)
+                    cropped_cutout = cropped.subclip(t_start = segment*length,t_end = endseg)
+                    cropped_cutout.write_videofile(cwd+'/'+video.split('.')[0]+'cropped_'+'part' +str(segment)+ '.mp4',codec = 'mpeg4',bitrate = "1500k",threads = 8)
+                except OSError as e:
+                    print('segment not viable')
+                gc.collect()
+        except OSError as e:
+
+             print(e.errno)
+             print('configuration not loaded')
+
+def write_cropped_video(cwd,video,interval,length,end): 
+    clip = VideoFileClip(cwd+'/'+video)
+    try:
+        with open(cwd+'/'+video.split('.')[0]+'config.py','r+') as f:
+            coords = ['x0 = \n','y0 = \n','x1 = \n','y1 = \n']
+            intcoords = []
+            for coord in range(len(coords)):
+                coords[coord] = f.readline()
+                nums = re.findall('\d+',coords[coord])[1]
+                intcoords.append(nums)
+
+        print(video.split('.')[0]+'cropped.avi')
+        cropped = clip.crop(x1 = intcoords[0],y1 = intcoords[1],x2 = intcoords[2],y2 = intcoords[3])
+        ## We want to split our video into manageable segments.
+        ## Account for the case that our video analysis failed somewhere in the middle:
+        # We want to be able to extract out the things that have been done so far:
+
+        for segment in interval:
+            print('moving to '+str(segment)+' of '+str(interval))
+            try:
+                # Ensures that the last clip is the right length
+                if segment == end: # Corner case for the last video segment. 
+
+                    endseg = -1
+                else:
+                    endseg = length*(segment+1)
+                cropped_cutout = cropped.subclip(t_start = segment*length,t_end = endseg)
+                cropped_cutout.write_videofile(cwd+'/'+video.split('.')[0]+'cropped_'+'part' +str(segment)+ '.mp4',codec = 'mpeg4',bitrate = "1500k",threads = 2,logger = None)
+
+            except OSError as e:
+                print('segment not viable')
+            gc.collect()
+    except OSError as e:
+
+         print(e.errno)
+         print('configuration not loaded')
+
+# No lambda functions are allowed in parallelized code, so we will use a function object instead. 
+class Renderer(object):
+    def __init__(self,cwd,video,length,end):
+        self.cwd = cwd 
+        self.video = video
+        self.length = length
+        self.end = end
+    def __call__(self,segments):
+        write_cropped_video(self.cwd,self.video,segments,self.length,self.end)
+
+def crop_videos_p(cwd = None):
+    # length in seconds of each video 
+    length = 1200
+    threads = 4
+    if cwd is None:
+        # First get current directory
+        cwd = os.getcwd()
+    # First get all subdirectories:
+    files = os.listdir(cwd)
+    videos = [video for video in files if video.split('.')[-1] == 'avi']
+    print(videos)
+    for video in videos:
+        ## Load in video
+        print('loading ' +video)
+        clip = VideoFileClip(cwd+'/'+video)
+        
+        ## First get the duration in seconds:
+        seconds = clip.duration
+        # If analysis has been found:
+        ident =  video.split('.')[0]+'cropped_'+'part'
+        print(files,ident)
+        print([part for part in files if ident in part.split('.')[0]])
+        done = [int(re.findall('\d+',part.split('.')[0])[-1]) for part in files if ident in part.split('.')[0]]
+        presegs = range(np.ceil(seconds/length).astype(int))
+        if len([part for part in files if ident in part.split('.')[0]]):
+            segments = [segment for segment in presegs if segment not in done]
+        else:
+            segments = presegs 
+        # This determines how we will split our resources: 
+        segments_split = index_segments(segments,threads)
+        p = multiprocessing.Pool()
+        ## We need to make a function object to get around the lack of lambda compatibility with multiprocessing: 
+        p.map(Renderer(cwd,video,length,presegs[-1]),segments_split)
+
+
+
+
+
+
+
+
 if __name__ == "__main__":
     cwd = sys.argv[1]
-    #crop_videos(cwd)
-    inspect_videos(cwd)
+    print(cwd,'outside')
+    crop_videos_p(cwd)
+    #inspect_videos(cwd)
