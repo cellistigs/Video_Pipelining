8d89b30fa18bc06d783944e274bdc44f800145a1
Ubuntu <ubuntu@ip-172-31-23-47.ec2.internal>
2019-04-09 14:28:38 +0000

    Last script for coordinating uploads

---
+++ segment_analysis.py
@@ -0,0 +1,13 @@
+## Script for automatic analysis post dlc tracking. 
+from Social_Dataset_Class import social_dataset
+import moviepy
+
+## Load in all relevant files
+
+## Analyze with accompanying movies (parallelize?)
+
+## Do tracking. 
+
+## Find shepherding events. 
+
+## Concatenate shepherding events. 
