e5ca35329fecea3a31210be60d55b917c614f56d
Ubuntu <ubuntu@ip-172-31-34-135.ec2.internal>
2019-11-04 14:10:53 +0000

    merging upload

--- Upload_S3.py
+++ Upload_S3.py
@@ -11,15 +11,16 @@ from Interface_S3 import upload
 
 if __name__ == "__main__":
     foldername = sys.argv[1]
+    bucket_name = sys.argv[2] 
     keypath = sys.argv[3]
     resultpath = sys.argv[4]
-    bucket_name = sys.argv[2] 
+    exclude = sys.argv[5]
     
     print(foldername,keypath,bucket_name)
     ## Only reupload analysis results:
     analysis_results = os.listdir(foldername)    
     for filename in analysis_results:
-        if filename.split('.')[-1] != 'mp4':
+        if filename.split('.')[-1] != exclude:
             ## give the file the right key prefix: 
             key = keypath+'/'+resultpath+'/'+filename 
             print(key,foldername+filename)
