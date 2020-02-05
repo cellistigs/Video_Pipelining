7581f1293ee8e2d1cd7e6faa3bd9410297ad5cfc
root <root@ip-172-31-42-71.ec2.internal>
2020-02-05 22:39:24 +0000

    included raw trajectories.

--- Carcea_Postprocess.py
+++ Carcea_Postprocess.py
@@ -1,6 +1,7 @@
 import sys
 import os
 import csv
+import datetime
 import numpy as np
 from Social_Dataset_Class_v2 import social_dataset_online
 from training_data.Training_Data_Class import training_dataset
@@ -56,6 +57,8 @@ if __name__ == "__main__":
             social_obj = social_dataset_online(os.path.join(datadirectory,tracefile),vers = 1)
             social_obj.set_nest(nestrel)
             social_obj.filter_full_new(trainpath)
+
+
             ## Now get trajectories: 
             trajectories = social_obj.render_trajectories(to_render = [i for i in range(10)])
             ## Get velocities:
@@ -64,11 +67,21 @@ if __name__ == "__main__":
             ## Get distance traveled: 
             centnorm = [np.linalg.norm(centvel,axis = 1) for centvel in velocities]
 
-            dist_traveled = [np.cumsum(cn) for cn in [0,5]]
+            dist_traveled = [np.cumsum(centnorm[cn]) for cn in [0,5]]
             
             ## Get ethograms:
             nest_ethos = [social_obj.nest_ethogram(m) for m in range(2)]
             pursuit_etho = social_obj.shepherding_ethogram()
+            ### Additionally get metrics on who is pursuing who. 
+            pursuit_premetric,direction= social_obj.orderedtracking()
+            ## Extract out the indices where pursuit happened: 
+            pursuit_indices = np.where(pursuit_etho)[0]
+            ## We can initialize the frame count via the time index parameter. 
+            ## TODO: share parameter for video length,frames per second w chunking with the video preprocessing. 
+            sec_init = int(tind[0]) * 2400
+            pursuit_frames = pursuit_indices
+            pursuit_secs = pursuit_frames/30 + sec_init 
+            pursuit_time = [str(datetime.timedelta(seconds=float(event))) for event in pursuit_secs]
 
             pos_lbs = ["tip","leftear","rightear","centroid","tailbase"]
             ind_lbs = ["virgin","dam"]
@@ -76,11 +89,21 @@ if __name__ == "__main__":
             for m,mouse in enumerate(ind_lbs):
                 for p,pos in enumerate(pos_lbs):
                     partind = m*5+p
+                    ## Added February 5th: 
+                    valid_locs, valid_times = social_obj.render_trajectory_valid(partind)
+                    ## Initialize array: 
+                    empty_array = np.nan*np.zeros((social_obj.dataset[social_obj.scorer].values.shape[0],2))
+                    print(valid_times.shape,empty_array.shape)
+                    empty_array[valid_times[:,0],:] = valid_locs
+                    all_dict["{}_{}_trajraw".format(mouse,pos)] = empty_array 
+                    #######################
                     all_dict["{}_{}_traj".format(mouse,pos)] = trajectories[partind]
                     all_dict["{}_{}_vel".format(mouse,pos)] = velocities[partind]
                 all_dict["{}_dist".format(mouse)] = dist_traveled[m]
                 all_dict["{}_nest".format(mouse)] = nest_ethos[m]
-            all_dict["pursuit".format(mouse)] = pursuit_etho
+            all_dict["pursuit_bool"] = pursuit_etho
+            all_dict["pursuit_direction"] = direction 
+            all_dict["pursuit_times"] = pursuit_time
 
             savemat(os.path.join(savedirectory,name+'processed.mat'),all_dict)
             #trajectories = social_obj.render_trajectories(to_render = [0,1,2,3,4,5,6,7,8,9])
@@ -92,6 +115,7 @@ if __name__ == "__main__":
             ##social_obj.full_ethogram(save = True,show = False,savepath = name+'/')
         except Exception as e: 
             print('Encountered exception '+str(e) +' while processing file '+str(tracefile))
+            raise NotImplementedError("Debugging")
       
         
     
