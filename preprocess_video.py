7ff896931e2cc3d08deca0e0e47b1598460982c0
Ubuntu <ubuntu@ip-172-31-95-203.ec2.internal>
2021-09-30 21:15:29 +0000

    added doc and extra arg for preprocess video

--- preprocess_video.py
+++ preprocess_video.py
@@ -1,12 +1,25 @@
-## Preprocessing script to be run as part of NCAP workflow for DLC
+# Preprocessing script to be run as part of NCAP workflow for DLC
 import sys
 from videotools import distribute_render
+"""Script to distribute rendering across multiple machines. 
+
+
+usage: 
+
+```
+python preprocess_video.py "dirpath" "configpath" "outpath"
+```
+
+where dirpath is a directory with videos, configpath is a path to a configuration file containing regions of interest, and outpath is the directory we should write outputs to. If outpath is none, will write back to directory. 
+
+"""
 
 if __name__ == "__main__":
    dirpath = sys.argv[1]
    configpath = sys.argv[2]
+   outpath = sys.argv[3]
    print('starting to render video')
-   distribute_render(configpath,dirpath)
+   distribute_render(configpath,dirpath,outpath)
    print('finishing video render')
 
 
